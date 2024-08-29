e3c545ac0b87afcd491211f268ec425061ef4831
Abdurazzoq3003 <aabdurazzoq80@gmail.com>
2024-08-29 12:18:19 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let str = "Abdurazzoq";
+let bool = true;
+let num = 30;
+let un = undefined;
+let nul = null;
+let big = 34n;
+let sym = Symbol;
+let obj = {};
+
+console.log(str);
+console.log(bool);
+console.log(num);
+console.log(un);
+console.log(nul);
+console.log(big);
+console.log(sym);
+console.log(obj);
